2117f012bab25285c609c6d16e30943f3a017dd5
Nataly-Costaa <natalynaty653@gmail.com>
2026-04-27 22:20:37 -0300

    atualizando

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'https://rede-solidaria-nine.vercel.app'
+    'https://rede-solidaria-three.vercel.app'
   ],
   credentials: true
 }));
